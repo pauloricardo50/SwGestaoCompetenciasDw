45c758c07a4b83771bf4cedd589286e67218bd87
Guilherme-Bodart <guilhermebodart73@gmail.com>
2020-08-26 23:09:41 -0300

    Card.jsx - Link no Titulo
    Colocando o titulo com Link para o nome do titulo
    
    Co-Authored-By: pauloricardo50 <26925333+pauloricardo50@users.noreply.github.com>

--- SwGestaoCompetenciasFront/src/components/Card/Card.jsx
+++ SwGestaoCompetenciasFront/src/components/Card/Card.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-
+import { NavLink } from "react-router-dom";
 export class Card extends Component {
   render() {
     return (
       <div className={"card" + (this.props.plain ? " card-plain" : "")}>
         <div className={"header" + (this.props.hCenter ? " text-center" : "")}>
-          <h4 className="title">{this.props.title}</h4>
+          <h4 className="title"><NavLink  to={this.props.title}>{this.props.title}</NavLink></h4>  
           <p className="category">{this.props.category}</p>
         </div>
         <div
