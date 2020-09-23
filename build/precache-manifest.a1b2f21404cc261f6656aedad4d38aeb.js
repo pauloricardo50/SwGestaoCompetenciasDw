b15b6c380cc214cded4e8b9f874516b00fcc6e1f
Guilherme-Bodart <guilhermebodart73@gmail.com>
2020-09-23 12:39:49 -0300

    Build
    Co-Authored-By: Jennifer Gonçalves do Amaral <jennifergamaral@gmail.com>
    Co-Authored-By: gmarchezi <26925551+gmarchezi@users.noreply.github.com>
    Co-Authored-By: pauloricardo50 <26925333+pauloricardo50@users.noreply.github.com>
    Co-Authored-By: Renato Lopes <38587847+renatosalopes@users.noreply.github.com>

---
+++ build/precache-manifest.a1b2f21404cc261f6656aedad4d38aeb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab15e657dcdf7cab638022850ca07b80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47f4b4da8cc386fa50db",
+    "url": "/static/css/2.daa9285a.chunk.css"
+  },
+  {
+    "revision": "65933bc719542b8ec907",
+    "url": "/static/css/main.ecfb6f5a.chunk.css"
+  },
+  {
+    "revision": "47f4b4da8cc386fa50db",
+    "url": "/static/js/2.6aa35138.chunk.js"
+  },
+  {
+    "revision": "cbd56acb272d799002d8108089ea48a4",
+    "url": "/static/js/2.6aa35138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65933bc719542b8ec907",
+    "url": "/static/js/main.1766eb90.chunk.js"
+  },
+  {
+    "revision": "7f493b3166c2536924d8",
+    "url": "/static/js/runtime-main.07bd00ab.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/static/media/sidebar-3.7bc259fd.jpg"
+  }
+]);
